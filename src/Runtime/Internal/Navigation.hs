a5f314410ebbfd0153be1b967a7ffabfc10ba9fe
John Cinnamond <john@cinnamond.me.uk>
2025-08-25 16:44:13 +0100

    wip: add specs for going back

--- src/Runtime/Internal/Navigation.hs
+++ src/Runtime/Internal/Navigation.hs
@@ -6,7 +6,8 @@ either by specifying a specific address or by searching for a named marker.
 -}
 module Runtime.Internal.Navigation where
 
-import Control.Monad.State (StateT, gets, modify)
+import Control.Monad.State (MonadState (get), StateT, gets, modify)
+import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.Vector qualified as V
 import Runtime.Internal.Types (Environment (..), Instruction (..), Runtime)
@@ -40,3 +41,35 @@ findMarker marker = do
         | x == y = True
         | otherwise = False
     find _ _ = False
+
+{- | Jump back in the presentation to the previous marker.
+
+If the previous jump back was to the previous marker then move the one prior
+to that. This stops the navigation getting stuck jumping back and then advancing
+by a few instructions.
+
+If there is no marker to jump back to then do nothing.
+-}
+goBack :: Runtime
+goBack = do
+    env <- get
+    let end = max 1 $ fromMaybe env.pc env.lastJump
+    let lastMarker = V.findIndexR isBackMarker $ V.slice 0 (end - 1) env.instructions
+    case lastMarker of
+        (Just i) -> do
+            modify (\e -> e{lastJump = Just i})
+            jump $ i + 1
+        Nothing -> modify (\e -> e{pc = 0, lastJump = Just 0})
+  where
+    isBackMarker :: Instruction -> Bool
+    isBackMarker StoreBackMarker = True
+    isBackMarker _ = False
+
+{- | StoreBackMarker is used to mark locations in the presentation for jumping
+back, typically by pressing 'b' or 'PgUp' when waiting for input.
+
+It is mostly ignored by the runtime until trying to decide where to jump back
+to, but when encountered it clears any previously stored last jump location.
+-}
+storeBackMarker :: Runtime
+storeBackMarker = modify (\e -> e{lastJump = Nothing})
